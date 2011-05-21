3f9a807dea2745d836943bdfa71620914e23bad0
Martin Kolman <martin.kolman@gmail.com>
2011-05-21 02:46:17 +0200

    add content

--- info.py
+++ info.py
@@ -3,24 +3,59 @@
 import gtk
 import os
 
+import hildon
+
 def getShortcutsContent():
   vbox = gtk.VBox()
-  shortcuts = gtk.Label("shortcuts")
+  shortcuts = gtk.Label()
+  text = "<b>Keyboard shortcuts</b>\n"
+  text+= "\n\n<b>Main</b>"
+  text+= "\n<b>f</b> - fullscreen"
+  text+= "\n<b>n</b> - open file chooser"
+  text+= "\n<b>p</b> - show paging dialog"
+  text+= "\n<b>m</b> - minimize the main window"
+  text+= "\n<b>k</b> - toggle kinetic scrolling"
+  text+= "\n<b>q</b> - quit"
+  text+= "\n<b>volume keys</b> - turn pages"
+  text+= "\n\n<b>Page fitting</b>"
+  text+= "\n<b>o</b> - original size"
+  text+= "\n<b>i</b> - fit to width"
+  text+= "\n<b>u</b> - fit to height"
+  text+= "\n<b>z</b> - fit to screen"
+  text+= "\n<b>doubletap</b> - temporary fit to screen"
+
+  shortcuts.set_markup(text)
+
   vbox.pack_start(shortcuts)
   vbox.show_all()
   return vbox
 
 def getStatsContent(mieru):
-  vbox = gtk.VBox()
+  vbox = gtk.VBox(False,0)
   statsText = mieru.stats.getStatsText()
-  stats = gtk.Label(statsText)
+  stats = gtk.Label()
+  stats.set_markup(statsText)
   vbox.pack_start(stats)
+  hbox = gtk.HBox()
+  statsOnOff = gtk.CheckButton("keep statistics")
+  resetStats = gtk.Button("Reset statistics")
+  hbox.pack_end(resetStats)
+  hbox.pack_end(statsOnOff)
+  vbox.pack_start(hbox, False, False, 5)
   vbox.show_all()
   return vbox
 
 def getAboutContent(versionString="unknown"):
-  vbox = gtk.VBox()
-  about = gtk.Label("Mieru version: %s" % versionString)
+  vbox = gtk.VBox(False,0)
+  about = gtk.Label()
+  text = "<b>Mieru</b> version: <b>%s</b>\n" % versionString
+  text+= "\nadd icon here\n"
+  text+= "\nContact the <b>Mieru</b> project:"
+  text+= "\nmain developer: <b>Martin Kolman</b>"
+  text+= "\nemail: <b>mieru.info@gmail.com</b>"
+  text+= "\nwww: add WWW :)"
+
+  about.set_markup(text)
   vbox.pack_start(about)
   vbox.show_all()
   return vbox
