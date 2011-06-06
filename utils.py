e21f38b253b5fb6bab5ef105f5bac2b0bd0d3d26
Martin Kolman <martin.kolman@gmail.com>
2011-06-06 05:05:17 +0200

    add a point class

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
 """various miscantelous utility functions for Mieru"""
 
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def add(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        print "(%d, %d)" % (self.x, self.y)
