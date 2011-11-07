7defdd71748f27053233b82dd1fa870f468a96d1
Martin Kolman <martin.kolman@gmail.com>
2011-11-07 14:40:18 +0100

    code cleanup

--- gui/qml_gui.py
+++ gui/qml_gui.py
@@ -64,20 +64,6 @@ class QMLGUI(gui.GUI):
 #    self.prevButton.clicked.connect(self._prevCB)
     self.toggleFullscreen()
 
-#    gui.GUI.__init__(self, mieru)
-#    if type == "hildon":
-#      import hildon
-#      self.window = hildon.StackableWindow()
-#    else:
-#      self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-#    self.resize(*size)
-#
-#    self.window.connect("destroy", self._destroyCB)
-#
-#    self.vbox = gtk.VBox(False, 2)
-#    self.window.add(self.vbox)
-#    self.fullscreen = False
-
 #  def resize(self, w, h):
 #    self.window.resize(w,h)
 #
