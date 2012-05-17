102b52975236e4a5b125f9c0a841e8f8f379f70e
Martin Kolman <martin.kolman@gmail.com>
2012-05-17 17:52:29 +0200

    progressive download, re-download

--- providers/bakatsuki.py
+++ providers/bakatsuki.py
@@ -6,6 +6,7 @@ import urllib2
 import sys
 #noinspection PyCompatibility
 import argparse
+import progressive_download
 
 
 URL_PREFIX = "http://www.baka-tsuki.org/project/index.php?title="
@@ -18,17 +19,33 @@ def getFullName(name, number, volumeName=VOLUME_NAME):
   return fullName
 
 def getUrl(fullName):
-  """
-  get Url for a given novel
-  """
+  """get Url for a given novel"""
   url = URL_PREFIX + fullName + PRINT_SUFFIX
   return url
+
+def downloadNovel(fullName):
+  """download a novel with the given fullname"""
+  url = getUrl(fullName)
+  # assure storage folder exists
+  if not os.path.exists(fullName):
+    os.mkdir(fullName)
+  filePath = os.path.join(fullName, "%s.html" % fullName)
+  downloadUrl(url, filePath)
+
+def downloadUrl(url, path):
+  progressive_download.download(url, path)
+
+
+def isLocallyAvailable(fullName):
+  """check if a novel with this full name is locally available"""
+  return os.path.exists(fullName)
 
 #def getStorageName(name, number, )
 
 if __name__ == "__main__":
+  # CLI argument parsing
   parser = argparse.ArgumentParser(description='Light novel processing.')
-  parser.add_argument('--name',
+  parser.add_argument('name',
     help="light novel name",
     action="store")
   parser.add_argument('--number',
@@ -37,18 +54,23 @@ if __name__ == "__main__":
   parser.add_argument('--volume',
     help="override volume name",
     action="store")
+  parser.add_argument('-r',
+    help="re-download even if locally available",
+    action="store_true")
   args = parser.parse_args()
+  # get the novel name
   name = args.name
   fullName = getFullName(name, 1)
-  print fullName
-  url = getUrl(fullName)
-  print url
-  result = urllib2.urlopen(url)
-  os.mkdir(fullName)
-  filePath = os.path.join(fullName, "%s.html" % fullName)
-  f = open(filePath, "w")
-  f.write(result.read())
-  f.close
+  print(fullName)
+  if isLocallyAvailable(fullName):
+    if args.r:
+      print("re-downloading")
+      downloadNovel(fullName)
+    else:
+      print("locally available")
+  else:
+    print("downloading")
+    downloadNovel(fullName)
 
 
 
---
+++ providers/progressive_download.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+downloading with progress information
+based on:
+http://stackoverflow.com/questions/2028517/python-urllib2-progress-hook
+"""
+import urllib2, sys
+
+def _chunk_report(bytes_so_far, chunk_size, total_size):
+  if total_size:
+    percent = float(bytes_so_far) / total_size
+    percent = round(percent*100, 2)
+    sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" %
+                     (bytes_so_far, total_size, percent))
+
+    if bytes_so_far >= total_size:
+      sys.stdout.write('\n')
+  else:
+    sys.stdout.write("Downloaded %d bytes\r" % bytes_so_far)
+
+
+def _chunk_download(url, path, chunk_size=8192, report_hook=None):
+  """
+  response hook = None -> select response hook automatically
+  response hook = False -> do not use response hook
+  """
+  response = urllib2.urlopen(url)
+  total_size = response.info().getheader('Content-Length')
+  if total_size:
+    total_size = total_size.strip()
+    total_size = int(total_size)
+  bytes_so_far = 0
+
+  f = open(path, "w")
+
+  while 1:
+    chunk = response.read(chunk_size)
+    bytes_so_far += len(chunk)
+    f.write(chunk)
+
+    if not chunk:
+      break
+
+    if report_hook is None:
+      _chunk_report(bytes_so_far, chunk_size, total_size)
+  f.close()
+
+  return bytes_so_far
+
+def download(url, path, report_hook=None, verbose=True):
+
+  if verbose:
+    print(url)
+  _chunk_download(url, path, report_hook=None)
+
+
+if __name__ == '__main__':
+  download("http://www.modrana.org/om2012/mobile_os_om2012.odp", "test.odp")
+
