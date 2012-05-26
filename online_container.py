e16612824092442a3a2041f6ded27b2f1be3bf13
Martin Kolman <martin.kolman@gmail.com>
2012-05-26 19:52:45 +0200

    online container skeleton

---
+++ online_container.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Mieru manga page online containers
+#
+#
+
+from container import Container
+
+class OnlineContainer(Container):
+  def __init__(self):
+    Container.init(None)
+
