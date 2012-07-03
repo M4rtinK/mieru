da18dfcd712d0d0138299b110081a7c6f45f4c5b
Martin Kolman <martin.kolman@gmail.com>
2012-07-04 00:23:03 +0200

    add incomplete spanish translation

---
+++ gui/qml/i18n/qml_spa.ts
@@ -0,0 +1,403 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>FileSelector</name>
+    <message>
+        <location filename="../FileSelector.qml" line="14"/>
+        <source>File Selector</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <location filename="../HistoryPage.qml" line="20"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="31"/>
+        <location filename="../HistoryPage.qml" line="118"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="54"/>
+        <source>Do not delete items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="54"/>
+        <source>Delete items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="57"/>
+        <source>Select items to delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="61"/>
+        <location filename="../HistoryPage.qml" line="115"/>
+        <source>Erase history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="74"/>
+        <source>no entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="116"/>
+        <source>Do you want to erase the history of all mangas and comic books opened by Mieru?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="117"/>
+        <source>Erase</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <location filename="../InfoPage.qml" line="14"/>
+        <source>Info</source>
+        <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="18"/>
+        <source>Stats</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="22"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="77"/>
+        <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="80"/>
+        <source>Pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="83"/>
+        <source>Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="91"/>
+        <source>Online search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="97"/>
+        <source>Opening &lt;b&gt;Google&lt;/b&gt; search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="106"/>
+        <source>Opening &lt;b&gt;Wikipedia&lt;/b&gt; search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="115"/>
+        <source>Opening &lt;b&gt;Manga updates&lt;/b&gt; search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="132"/>
+        <source>Usage Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="162"/>
+        <location filename="../InfoPage.qml" line="244"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="205"/>
+        <source>Mieru is a flexible Manga and comic book reader.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="211"/>
+        <source>main developer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="214"/>
+        <source>email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="218"/>
+        <source>www</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="223"/>
+        <source>discussion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="228"/>
+        <source>Donate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="242"/>
+        <source>Reset all usage statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="243"/>
+        <source>Do you really want to reset all usage statistics?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="245"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../MainView.qml" line="156"/>
+        <source>original</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="158"/>
+        <source>width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="160"/>
+        <source>height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="162"/>
+        <source>screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="164"/>
+        <source>orient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="171"/>
+        <source>most</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="179"/>
+        <source>custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="238"/>
+        <source>Open file</source>
+        <translation type="unfinished">Abrir</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="245"/>
+        <source>History</source>
+        <translation type="unfinished">Historia</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="249"/>
+        <source>Info</source>
+        <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="253"/>
+        <source>Options</source>
+        <translation type="unfinished">Configuratión</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="260"/>
+        <source>Quit</source>
+        <translation type="unfinished">Serar</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="459"/>
+        <source>Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="479"/>
+        <source>No pages loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OptionsPage</name>
+    <message>
+        <location filename="../OptionsPage.qml" line="25"/>
+        <source>Page view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="28"/>
+        <source>Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="43"/>
+        <source>auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="51"/>
+        <source>portrait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="59"/>
+        <source>landscape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="67"/>
+        <source>Show status bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="75"/>
+        <source>Remember toolbar state</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="82"/>
+        <source>Fullscreen button opacity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="108"/>
+        <source>Paging options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="111"/>
+        <source>Paging mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="126"/>
+        <source>Whole screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="134"/>
+        <source>On edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="142"/>
+        <source>Show paging feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="152"/>
+        <source>Page scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="155"/>
+        <source>Page fit mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="164"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="167"/>
+        <source>Manga reading mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PageFitSelector</name>
+    <message>
+        <location filename="../PageFitSelector.qml" line="10"/>
+        <source>Page fit mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="18"/>
+        <source>&lt;b&gt;1:1&lt;/b&gt; - original size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="19"/>
+        <source>&lt;b&gt;fit to width&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="20"/>
+        <source>&lt;b&gt;fit to height&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="21"/>
+        <source>&lt;b&gt;fit to screen&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="22"/>
+        <source>&lt;b&gt;custom&lt;/b&gt; - remember scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="23"/>
+        <source>&lt;b&gt;orientation&lt;/b&gt; specific</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="24"/>
+        <source>&lt;b&gt;show the most&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="83"/>
+        <source>How to turn pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="84"/>
+        <source>Tap the &lt;b&gt;right half&lt;/b&gt; of the screen to go to the &lt;b&gt;next page&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="85"/>
+        <source>Tap the &lt;b&gt;left half&lt;/b&gt; to go to the &lt;b&gt;previous page&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="86"/>
+        <source>Don&apos;t show again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="87"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
