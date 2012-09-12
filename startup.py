ab3dd8ae4f35dfa00317928f3c71694793013b62
Martin Kolman <martin.kolman@gmail.com>
2012-09-12 14:23:47 +0200

    documentation

--- startup.py
+++ startup.py
@@ -8,16 +8,19 @@ class Startup():
   def __init__(self):
     parser = argparse.ArgumentParser(description="A flexible manga and comic book reader.")
     parser.add_argument('-u',
-                        help="specify user interface type", default=None,
-                        action="store", choices=["pc","hildon", "harmattan", "QML"])
+      help="specify user interface type", default=None,
+      action="store", choices=["pc", "hildon", "harmattan", "QML"])
     parser.add_argument('-p',
-                        help="specify user interface type", default=None,
-                        action="store", choices=["maemo5", "harmattan", "pc"])
+      help="specify user interface type", default=None,
+      action="store", choices=["maemo5", "harmattan", "pc"])
     parser.add_argument('-o',
-                        help="specify path to a manga or comic book to open", default=None,
-                        action="store", metavar="path to file",)
+      help="specify path to a manga or comic book to open", default=None,
+      action="store", metavar="path to file", )
+    # TODO: option for setting the first page of the manga to be opened
+    # -> this could by also used to override the automatic history checking
+    # eq. by using 1 as the page number
     parser.add_argument('--locale',
-                        help="override system locale", default=None,
-                        action="store", metavar="language code",)
+      help="override system locale", default=None,
+      action="store", metavar="language code", )
     self.args = parser.parse_args()
 
