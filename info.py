55e20ffe5e0eae3621dbcbff689f6b172786d57e
Martin Kolman <martin.kolman@gmail.com>
2013-01-20 13:35:18 +0100

    Python 3 support

--- info.py
+++ info.py
@@ -6,27 +6,29 @@ import os
 VERSION_FILE_PATH = "version.txt"
 RELEASE_NOTES_FILE_PATH = "release_notes.txt"
 
+
 def getShortcutsContent():
   import gtk
+
   vbox = gtk.VBox()
   shortcuts = gtk.Label()
   text = "<b><u>Keyboard shortcuts</u></b>\n"
-  text+= "\n<b>Main</b>"
-  text+= "\n<b>f</b> - fullscreen"
-  text+= "\n<b>n</b> - open file chooser"
-  text+= "\n<b>p</b> - show paging dialog"
-  text+= "\n<b>c</b> - show show options window"
-  text+= "\n<b>a</b> - show info window"
-  text+= "\n<b>m</b> - minimize the main window"
-  text+= "\n<b>k</b> - toggle kinetic scrolling"
-  text+= "\n<b>q</b> - quit"
-  text+= "\n<b>volume keys</b> - turn page"
-  text+= "\n\n<b>Page fitting</b>"
-  text+= "\n<b>o</b> - original size"
-  text+= "\n<b>i</b> - fit to width"
-  text+= "\n<b>u</b> - fit to height"
-  text+= "\n<b>z</b> - fit to screen"
-  text+= "\n<b>double-tap</b> - temporary fit to screen"
+  text += "\n<b>Main</b>"
+  text += "\n<b>f</b> - fullscreen"
+  text += "\n<b>n</b> - open file chooser"
+  text += "\n<b>p</b> - show paging dialog"
+  text += "\n<b>c</b> - show show options window"
+  text += "\n<b>a</b> - show info window"
+  text += "\n<b>m</b> - minimize the main window"
+  text += "\n<b>k</b> - toggle kinetic scrolling"
+  text += "\n<b>q</b> - quit"
+  text += "\n<b>volume keys</b> - turn page"
+  text += "\n\n<b>Page fitting</b>"
+  text += "\n<b>o</b> - original size"
+  text += "\n<b>i</b> - fit to width"
+  text += "\n<b>u</b> - fit to height"
+  text += "\n<b>z</b> - fit to screen"
+  text += "\n<b>double-tap</b> - temporary fit to screen"
 
   shortcuts.set_markup(text)
 
@@ -34,22 +36,27 @@ def getShortcutsContent():
   vbox.show_all()
   return vbox
 
+
 def _updateStatsText(stats, label):
   statsText = stats.getStatsText()
   label.set_markup(statsText)
+
 
 def _wipeStatsCB(button, stats, label):
   stats.resetStats()
   _updateStatsText(stats, label)
 
+
 def _setStatsOnCB(button, stats, label):
   stats.setOn(button.get_active())
   _updateStatsText(stats, label)
 
+
 def getStatsContent(mieru):
   """get content for the statistics monitoring tab"""
   import gtk
-  vbox = gtk.VBox(False,0)
+
+  vbox = gtk.VBox(False, 0)
   statsText = mieru.stats.getStatsText()
   statsLabel = gtk.Label()
   statsLabel.set_markup(statsText)
@@ -69,26 +76,29 @@ def getStatsContent(mieru):
   vbox.show_all()
   return vbox
 
+
 def getAboutText(forum="meego"):
   #text= "<b>Mieru project</b> contact info:"
-  text= "<p><b>main developer:</b> Martin Kolman</p>"
-  text+= '<p><b>email</b>: <a href="mailto:mieru.info@gmail.com">mieru.info@gmail.com</a></p>'
-  text+= '<p><b>www</b>:  <a href="http://m4rtink.github.com/mieru/">http://m4rtink.github.com/mieru/</a></p>'
+  text = "<p><b>main developer:</b> Martin Kolman</p>"
+  text += '<p><b>email</b>: <a href="mailto:mieru.info@gmail.com">mieru.info@gmail.com</a></p>'
+  text += '<p><b>www</b>:  <a href="http://m4rtink.github.com/mieru/">http://m4rtink.github.com/mieru/</a></p>'
   if forum == "meego":
-    text+= '<p><b>discussion</b>: <a href="http://forum.meego.com/showthread.php?t=5405">forum.meego.com thread</a></p>'
+    text += '<p><b>discussion</b>: <a href="http://forum.meego.com/showthread.php?t=5405">forum.meego.com thread</a></p>'
   else:
-    text+= '<p><b>discussion</b>: check <a href="http://talk.maemo.org/showthread.php?t=73907">talk.maemo.org thread</a></p>'
+    text += '<p><b>discussion</b>: check <a href="http://talk.maemo.org/showthread.php?t=73907">talk.maemo.org thread</a></p>'
   return text
+
 
 def getAboutContent(versionString="unknown"):
   import gtk
-  vbox = gtk.VBox(False,0)
+
+  vbox = gtk.VBox(False, 0)
   textVersion = "<b>Mieru</b>, version: <b>%s</b>" % versionString
   about0 = gtk.Label()
   about0.set_markup(textVersion)
 
   mieruIcon = gtk.image_new_from_file('icons/mieru_150x150.png')
-#  mieruIcon.set_pixel_size(200)
+  #  mieruIcon.set_pixel_size(200)
   about1 = gtk.Label()
   about1.set_markup(getAboutText())
   vbox.pack_start(about0)
@@ -96,6 +106,7 @@ def getAboutContent(versionString="unknown"):
   vbox.pack_start(about1)
   vbox.show_all()
   return vbox
+
 
 def getVersionString():
   """Return current version string
@@ -112,10 +123,11 @@ def getVersionString():
       f.close()
       # is it really string ?
       versionString = str(versionString)
-    except Exception, e:
+    except Exception as e:
       print("loading version info failed")
       print(e)
   return versionString
+
 
 def getVersionNumber():
   """return version as a (primary, secondary, build) tuple
@@ -132,10 +144,11 @@ def getVersionNumber():
       versionString = versionString.split(' ')[0] # drop git tag
       c1, c2, c3 = versionString.split('.')
       return int(c1), int(c2), int(c3)
-    except Exception, e:
+    except Exception as e:
       print('info: parsing version number failed')
       print(e)
       return None
+
 
 def getNumericVersionString():
   versionNumber = getVersionNumber()
@@ -151,9 +164,11 @@ def nvs2tuple(nvString):
   a, b, c = nvString.split('.')
   return int(a), int(b), int(c)
 
+
 def tuple2nvs(versionTuple):
   """numeric version tuple to numeric version string"""
   return "%d.%d.%d" % versionTuple
+
 
 def getReleaseNotes():
   """return release notes fir the given version string
@@ -167,9 +182,10 @@ def getReleaseNotes():
   versionNumbers = None
   try:
     from modules.configobj import ConfigObj
+
     releaseNotes = ConfigObj(RELEASE_NOTES_FILE_PATH)
     versionNumbers = releaseNotes['release_notes_section'].keys()
-  except Exception, e:
+  except Exception as e:
     print('info: loading release notes failed')
     print(e)
   if versionNumbers:
@@ -178,12 +194,14 @@ def getReleaseNotes():
     notesMarkdown = releaseNotes['release_notes_section'][tuple2nvs(maxVersion)]['notes']
     try:
       import modules.markdown as markdown
+
       notesHTML = markdown.markdown(notesMarkdown)
-    except Exception, e:
+    except Exception as e:
       print('info: parsing release notes failed')
       #print(e)
       import traceback
       import sys
+
       traceback.print_exc(file=sys.stdout)
 
       return None, None
@@ -191,6 +209,7 @@ def getReleaseNotes():
     return maxVersion, notesHTML
   else:
     return None, None
+
 
 def getGitHash():
   """return Git hash for the current version
@@ -207,20 +226,24 @@ def getGitHash():
       versionString = versionString.split(' ')[1] # drop the version
       gitHash = versionString.split(':')[1] # drop the git: prefix
       return gitHash
-    except Exception, e:
+    except Exception as e:
       print('info: parsing version number failed')
       print(e)
       return None
 
+
 def _getLabel(name, spacing=0):
   import gtk
+
   vbox = gtk.VBox(False, spacing)
-  vbox.pack_start(gtk.Label(name),padding=spacing)
+  vbox.pack_start(gtk.Label(name), padding=spacing)
   vbox.show_all()
   return vbox
 
+
 def getInfNotebook(mieru):
   import gtk
+
   class InfoNotebook(gtk.Notebook):
     def __init__(self, mieru):
       gtk.Notebook.__init__(self)
@@ -228,11 +251,12 @@ def getInfNotebook(mieru):
       self.versionString = getVersionString()
       enlargeTabs = 15
       versionString = self.getVersionString()
-      self.append_page(getShortcutsContent(),_getLabel("Shortcuts",enlargeTabs))
-      self.append_page(getStatsContent(self.mieru),_getLabel("Stats", enlargeTabs))
-      self.append_page(getAboutContent(versionString),_getLabel("About", enlargeTabs))
+      self.append_page(getShortcutsContent(), _getLabel("Shortcuts", enlargeTabs))
+      self.append_page(getStatsContent(self.mieru), _getLabel("Stats", enlargeTabs))
+      self.append_page(getAboutContent(versionString), _getLabel("About", enlargeTabs))
       self.show_all()
       self.set_current_page(0)
+
   return InfoNotebook(mieru)
 
 
