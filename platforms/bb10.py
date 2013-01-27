34faf838cfdb7113458a4e7c9c3b7cc8e45340c8
Martin Kolman <martin.kolman@gmail.com>
2013-01-27 02:06:20 +0100

    core file cleanup (cherry picked from commit d0e3133)

--- platforms/bb10.py
+++ platforms/bb10.py
@@ -2,14 +2,16 @@
 """
 Mieru hildon UI (for Maemo 5@N900)
 """
-
-from base_platform import BasePlatform
+import glob
+import os
+from platforms.base_platform import BasePlatform
 
 
 class BB10(BasePlatform):
   def __init__(self, mieru):
     BasePlatform.__init__(self)
     self.mieru = mieru
+    self._cleanCoreDumps()
 
   def getIDString(self):
     return "bb10"
@@ -26,7 +28,7 @@ class BB10(BasePlatform):
   def getDefaultFileSelectorPath(self):
     """we default to the MyDocs folder as this is where most
     users will store their mangas and comic books"""
-    return "."
+    return "/accounts/1000/shared/downloads"
 
   def showQuitButton(self):
     """Swype handles window closing"""
@@ -41,3 +43,11 @@ class BB10(BasePlatform):
 
   def startInFullscreen(self):
     return True
+
+  def _cleanCoreDumps(self):
+    """due to experimental OpenGL support when using QtQuick with QBB_USE_OPENGL,
+    there might sometimes be creates python3.2 core dumps even though the program cleanly exits
+    -> this function cleans them on startup"""
+    corePath = os.path.join(os.environ['SANDBOX'], 'logs', '*.core')
+    for core in glob.glob(corePath):
+      os.remove(core)
