5c9aeb8a44948c11a7d7b96d58848da8927edefb
Martin Kolman <martin.kolman@gmail.com>
2013-05-31 18:42:39 +0200

    Add support for listing novels from Bakatsuki

--- providers/bakatsuki.py
+++ providers/bakatsuki.py
@@ -2,16 +2,21 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-import urllib2
-import sys
+try:  # Python 2
+  from urllib2 import urlopen, HTTPError, URLError
+except ImportError:  # Python 3
+  from urllib.request import urlopen
+  from urllib.error import HTTPError, URLError
 #noinspection PyCompatibility
 import argparse
 import progressive_download
 
 
 URL_PREFIX = "http://www.baka-tsuki.org/project/index.php?title="
+MAIN_PAGE = "http://www.baka-tsuki.org/project/index.php?title=Main_Page"
 VOLUME_NAME = "Volume"
 PRINT_SUFFIX = "&printable=yes"
+NOVEL_LIST_VALID = 24 # in hours
 
 def getFullName(name, number, volumeName=VOLUME_NAME):
   fullName = re.sub(" ", "_", name)
@@ -40,6 +45,35 @@ def isLocallyAvailable(fullName):
   """check if a novel with this full name is locally available"""
   return os.path.exists(fullName)
 
+def listAllAvailableNovels():
+  """list all novels available from Bakatsuki"""
+  LN = 0
+  h = urlopen(MAIN_PAGE)
+  novels = []
+  for i in range(500):
+    data = h.readline()
+    if re.search(r"- Light Novels -->",str(data)) is not None:
+      LN = 1
+    if re.search(r"- /Light Novels -->",str(data)) is not None:
+      LN = 0
+
+    #print (re.search(r"- Light Novels -->",str(data)))
+    #data = data.decode('utf-8', errors='replace')
+    # print (str(data))
+    #found = re.findall(".*title\=.*\">.*</a></li>",str(data))
+    if LN == 1:
+      #found = re.findall("index.php\?title\=.*\">",str(data))
+      found = re.search("index.php\?title\=(.*)\">", str(data))
+      # found = re.findall("index.php\?title\=.*\">",str(data))
+      # purge the prefix
+      #found = re.sub("index.php\?title\=.*\">", "", found)
+      if found:
+        novels.append(found.group(1))
+
+  h.close()
+  for novel in novels:
+    print(novel.replace("_"," "))
+
 #def getStorageName(name, number, )
 
 if __name__ == "__main__":
@@ -47,7 +81,9 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Light novel processing.')
   parser.add_argument('name',
     help="light novel name",
-    action="store")
+    action="store",
+    nargs='?',
+    default=None)
   parser.add_argument('--number',
     help="volume number",
     action="store")
@@ -57,20 +93,26 @@ if __name__ == "__main__":
   parser.add_argument('-r',
     help="re-download even if locally available",
     action="store_true")
+  parser.add_argument('--list',
+    help="list all available light novels",
+    action="store_true")
   args = parser.parse_args()
-  # get the novel name
-  name = args.name
-  fullName = getFullName(name, 1)
-  print(fullName)
-  if isLocallyAvailable(fullName):
-    if args.r:
-      print("re-downloading")
-      downloadNovel(fullName)
-    else:
-      print("locally available")
+  # get name of the novel
+  if args.list:
+    listAllAvailableNovels()
   else:
-    print("downloading")
-    downloadNovel(fullName)
+    name = args.name
+    fullName = getFullName(name, 1)
+    print(fullName)
+    if isLocallyAvailable(fullName):
+      if args.r:
+        print("re-downloading")
+        downloadNovel(fullName)
+      else:
+        print("locally available")
+    else:
+      print("downloading")
+      downloadNovel(fullName)
 
 
 
