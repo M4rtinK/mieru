e9ef77c5ef8c7d593e66bb69e48681291395fb79
Martin Kolman <martin.kolman@gmail.com>
2013-07-12 21:11:39 +0200

    Add full Greek and partial Spanish translation

---
+++ gui/qml/i18n/qml_el.ts
@@ -0,0 +1,501 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="el" version="2.0">
+<context>
+    <name>BitcoinButton</name>
+    <message>
+        <location filename="../BitcoinButton.qml" line="39"/>
+        <source>Bitcoin address</source>
+        <translation>Bitcoin διεύθυνση</translation>
+    </message>
+    <message>
+        <location filename="../BitcoinButton.qml" line="116"/>
+        <source>Copy address</source>
+        <translation>Αντιγραφή διεύθυνσης</translation>
+    </message>
+    <message>
+        <location filename="../BitcoinButton.qml" line="121"/>
+        <source>Bitcoin address copied to clipboard</source>
+        <translation>Bitcoin διεύθυνση αντιγραφή στο πρόχειρο</translation>
+    </message>
+</context>
+<context>
+    <name>DonationDialog</name>
+    <message>
+        <location filename="../DonationDialog.qml" line="8"/>
+        <source>Choose a donation method:</source>
+        <translation>Επιλέξτε μια μέθοδο δωρεάς:</translation>
+    </message>
+</context>
+<context>
+    <name>FileSelector</name>
+    <message>
+        <location filename="../FileSelector.qml" line="14"/>
+        <source>File Selector</source>
+        <translation>Επιλογέας αρχείου</translation>
+    </message>
+</context>
+<context>
+    <name>FlattrButton</name>
+    <message>
+        <location filename="../FlattrButton.qml" line="26"/>
+        <source>Opening &lt;b&gt;Flattr&lt;/b&gt; donation page, &lt;b&gt;thanks&lt;/b&gt;!</source>
+        <translation>Άνοιγμα της &lt;/b&gt;Flattr&lt;/b&gt; σελίδας δωρεων, &lt;/b&gt;ευχαριστω&lt;/b&gt;!</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <location filename="../HistoryPage.qml" line="20"/>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="31"/>
+        <location filename="../HistoryPage.qml" line="118"/>
+        <source>Cancel</source>
+        <translation>Ακύρωση</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="54"/>
+        <source>Do not delete items</source>
+        <translation>Καμμια διαγραφή στοιχείων</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="54"/>
+        <source>Delete items</source>
+        <translation>Διαγραφή στοιχείων</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="57"/>
+        <source>Select items to delete</source>
+        <translation>Επιλογή στοιχείων για διαγραφή</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="61"/>
+        <location filename="../HistoryPage.qml" line="115"/>
+        <source>Erase history</source>
+        <translation>Διαγραφή ιστορικόυ</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="74"/>
+        <source>no entries</source>
+        <translation>δεν υπάρχουν καταχωρήσεις</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="116"/>
+        <source>Do you want to erase the history of all mangas and comic books opened by Mieru?</source>
+        <translation>Θέλετε να διαγράψετε το ιστορικό όλων των μάγκας και κόμικς που εχει ανοίξει το Mieru;</translation>
+    </message>
+    <message>
+        <location filename="../HistoryPage.qml" line="117"/>
+        <source>Erase</source>
+        <translation>Διαγραφή</translation>
+    </message>
+</context>
+<context>
+    <name>InfoPage</name>
+    <message>
+        <location filename="../InfoPage.qml" line="14"/>
+        <source>Info</source>
+        <translation>Πληροφορία</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="18"/>
+        <source>Stats</source>
+        <translation>Στατιστικά</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="22"/>
+        <source>About</source>
+        <translation>Σχετικά με</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="77"/>
+        <source>Properties</source>
+        <translation>Ιδιότητες</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="80"/>
+        <source>Pages</source>
+        <translation>Σελίδες</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="83"/>
+        <source>Path</source>
+        <translation>Διαδρομή</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="91"/>
+        <source>Online search</source>
+        <translation>Διαδικτυακη αναζήτηση</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="97"/>
+        <source>Opening &lt;b&gt;Google&lt;/b&gt; search</source>
+        <translation>Ανοιγμα με το &lt;b&gt;Google&lt;/b&gt; search</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="106"/>
+        <source>Opening &lt;b&gt;Wikipedia&lt;/b&gt; search</source>
+        <translation>Ανοιγμα με το &lt;b&gt;Wikipedia&lt;/b&gt; search</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="115"/>
+        <source>Opening &lt;b&gt;Manga updates&lt;/b&gt; search</source>
+        <translation>Ανοιγμα με το &lt;b&gt;Manga updates&lt;/b&gt; search</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="132"/>
+        <source>Usage Statistics</source>
+        <translation>Στατιστικά χρήσης</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="162"/>
+        <location filename="../InfoPage.qml" line="284"/>
+        <source>Reset</source>
+        <translation>Επαναφορά</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="213"/>
+        <source>Mieru is a flexible Manga and comic book reader.</source>
+        <translation>Το Mieru είναι ένας ευέλικτο Manga και κόμικς αναγνώστης βιβλίων.</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="223"/>
+        <source>&lt;b&gt;Do you like Mieru ? Donate !&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Σας αρέσει το Mieru; Δωρίστε !&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="256"/>
+        <source>main developer</source>
+        <translation>κυρίως προγραμματιστής</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="259"/>
+        <source>email</source>
+        <translation>email</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="263"/>
+        <source>www</source>
+        <translation>www</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="268"/>
+        <source>discussion</source>
+        <translation>συζήτηση</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="282"/>
+        <source>Reset all usage statistics</source>
+        <translation>Επαναφορά όλων των στατιστικών στοιχείων χρήσης</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="283"/>
+        <source>Do you really want to reset all usage statistics?</source>
+        <translation>Είστε σίγουροι ότι θέλετε να επαναφέρετε όλα τα στατιστικά στοιχεία χρήσης;</translation>
+    </message>
+    <message>
+        <location filename="../InfoPage.qml" line="285"/>
+        <source>Cancel</source>
+        <translation>Ακύρωση</translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../MainView.qml" line="169"/>
+        <source>original</source>
+        <translation>πρωτότυπο</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="171"/>
+        <source>width</source>
+        <translation>πλάτος</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="173"/>
+        <source>height</source>
+        <translation>ύψος</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="175"/>
+        <source>screen</source>
+        <translation>οθόνη</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="177"/>
+        <source>orient</source>
+        <translation>προσανατολίσμος</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="184"/>
+        <source>most</source>
+        <translation>περισσότερο</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="192"/>
+        <source>custom</source>
+        <translation>προσαρμοσμένο</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="250"/>
+        <source>Open file</source>
+        <translation>Άνοιγμα αρχείου</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="257"/>
+        <source>History</source>
+        <translation>Ιστορικό</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="261"/>
+        <source>Info</source>
+        <translation>Πληροφορία</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="265"/>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="272"/>
+        <source>Quit</source>
+        <translation>Αποχωρηση</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="504"/>
+        <source>Rotation</source>
+        <translation>Περιστροφή</translation>
+    </message>
+    <message>
+        <location filename="../MainView.qml" line="524"/>
+        <source>No pages loaded</source>
+        <translation>Δεν υπάρχουν σελίδες που φορτώνονται</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsPage</name>
+    <message>
+        <location filename="../OptionsPage.qml" line="29"/>
+        <source>Page view</source>
+        <translation>Προβολή σελίδας</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="32"/>
+        <source>Rotation</source>
+        <translation>Περιστροφή</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="47"/>
+        <source>auto</source>
+        <translation>αυτόματο</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="55"/>
+        <source>portrait</source>
+        <translation>Πορτρέτο</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="63"/>
+        <source>landscape</source>
+        <translation>Τοπίο</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="71"/>
+        <source>Show status bar</source>
+        <translation>Εμφάνιση γραμμής κατάστασης</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="79"/>
+        <source>Remember toolbar state</source>
+        <translation>Εκθυμηση της κατάστασης γραμμής εργαλείων</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="86"/>
+        <source>Fullscreen button opacity</source>
+        <translation>Πλήρης οθόνη αδιαφάνεια κουμπιού</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="112"/>
+        <source>Paging options</source>
+        <translation>Επιλογές σελιδοποίησης</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="115"/>
+        <source>Paging mode</source>
+        <translation>Λειτουργία σελιδοποίησης</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="130"/>
+        <source>Whole screen</source>
+        <translation>Ολόκληρη την οθόνη</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="138"/>
+        <source>On edges</source>
+        <translation>Στις άκρες</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="148"/>
+        <source>Middle click</source>
+        <translation>Μεσαίο κλικ</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="158"/>
+        <source>Middle doubleclick</source>
+        <translation>Μεσαίο διπλό κλικ</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="168"/>
+        <source>Show paging feedback</source>
+        <translation>Εμφάνιση σχολίων τηλεειδοποίησης</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="178"/>
+        <source>Page scaling</source>
+        <translation>Κλιμάκωση σελίδας</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="181"/>
+        <source>Page fit mode</source>
+        <translation>Σελίδα κατάστασης προσαρμογής</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="190"/>
+        <source>History</source>
+        <translation>Ιστορικό</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="193"/>
+        <source>history enabled</source>
+        <translation>Ενεργοποιηση ιστορικου</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="201"/>
+        <source>use when opening files</source>
+        <translation>Χρησιμοποιήση κατά το άνοιγμα αρχείων</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="210"/>
+        <source>Miscellaneous</source>
+        <translation>Διάφορα</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="213"/>
+        <source>Manga reading mode</source>
+        <translation>Manga τρόπος ανάγνωσης</translation>
+    </message>
+    <message>
+        <location filename="../OptionsPage.qml" line="221"/>
+        <source>Show release notes</source>
+        <translation>Εμφάνιση σημειώσεων έκδοσης</translation>
+    </message>
+</context>
+<context>
+    <name>PageFitSelector</name>
+    <message>
+        <location filename="../PageFitSelector.qml" line="10"/>
+        <source>Page fit mode</source>
+        <translation>Σελίδα κατάστασης προσαρμογής</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="19"/>
+        <source>&lt;b&gt;1:1&lt;/b&gt; - original size</source>
+        <translation>&lt;b&gt;1:1&lt;/b&gt; - αρχικό μέγεθος</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="20"/>
+        <source>&lt;b&gt;fit to width&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Προσαρμογή στο πλάτος&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="21"/>
+        <source>&lt;b&gt;fit to height&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Προσαρμογή στο ύψος&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="22"/>
+        <source>&lt;b&gt;fit to screen&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Προσαρμογή στην οθονη&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="23"/>
+        <source>&lt;b&gt;custom&lt;/b&gt; - remember scale</source>
+        <translation>&lt;b&gt;προσαρμοσμένη&lt;b&gt; - κλίμακα</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="24"/>
+        <source>&lt;b&gt;orientation&lt;/b&gt; specific</source>
+        <translation>&lt;b&gt;προσανατολισμός&lt;/b&gt;  συγκεκριμένων</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="25"/>
+        <source>&lt;b&gt;show the most&lt;/b&gt;</source>
+        <translation>&lt;b&gt;δείτε τα πιο&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PageFitSelector.qml" line="29"/>
+        <source>&lt;b&gt;no action&lt;/b&gt;</source>
+        <translation>&lt;b&gt;καμία ενέργεια&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>PayPalButton</name>
+    <message>
+        <location filename="../PayPalButton.qml" line="25"/>
+        <source>Opening &lt;b&gt;PayPal&lt;/b&gt; donation page, &lt;b&gt;thanks&lt;/b&gt;!</source>
+        <translation>Άνοιγμα της &lt;/b&gt;PayPal&lt;/b&gt; σελίδας δωρεων, &lt;/b&gt;ευχαριστω&lt;/b&gt;!</translation>
+    </message>
+</context>
+<context>
+    <name>WhatsNewDialog</name>
+    <message>
+        <location filename="../WhatsNewDialog.qml" line="35"/>
+        <source>Opening link</source>
+        <translation>Ανοιγμα συνδέσμου</translation>
+    </message>
+    <message>
+        <location filename="../WhatsNewDialog.qml" line="66"/>
+        <source>&lt;b&gt;Do you like Mieru ? Donate !&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Σας αρέσει το Mieru; Δωρίστε !&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../WhatsNewDialog.qml" line="74"/>
+        <source>How to donate ?</source>
+        <translation>Πώς να δωρίσετε;</translation>
+    </message>
+    <message>
+        <location filename="../WhatsNewDialog.qml" line="85"/>
+        <source>Don&apos;t show again</source>
+        <translation>Να μην εμφανιστεί ξανά</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="96"/>
+        <source>How to turn pages</source>
+        <translation>Πώς να στραφούν οι σελίδες</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="97"/>
+        <source>Tap the &lt;b&gt;right half&lt;/b&gt; of the screen to go to the &lt;b&gt;next page&lt;/b&gt;.</source>
+        <translation>Πατήστε το &lt;b&gt;δεξί μισό&lt;b&gt; της οθόνης για να μεταβείτε στην &lt;b&gt;επόμενη σελίδα&lt;b&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="98"/>
+        <source>Tap the &lt;b&gt;left half&lt;/b&gt; to go to the &lt;b&gt;previous page&lt;/b&gt;.</source>
+        <translation>Πατήστε το &lt;b&gt;αριστερό μισό&lt;b&gt; για να μεταβείτε στην &lt;b&gt;προηγούμενη σελίδα&lt;b&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="99"/>
+        <source>Don&apos;t show again</source>
+        <translation>Να μην εμφανιστεί ξανά</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="100"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
